ded8b56fad1eb7e0cae2ff3fc6c53395a3de64a8
Manuel <manuel.simon.novoa@gmail.com>
2019-06-23 16:40:55 +0200

    Implementada función para modal

---
+++ js/modal.js
@@ -0,0 +1,4 @@
+var refs = {
+    open: function(a) { document.getElementById(a).classList.add('is-active');  },
+    close:  function(a) { document.getElementById(a).classList.remove('is-active');   }
+};
